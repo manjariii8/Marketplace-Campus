63b3610e00ff913c8808350e0fe42f21af294c83
Manjari <manjari.tripathi.tech@gmail.com>
2026-09-07 00:10:27 +0530

    Update CORS CONFIG

--- backend/src/main/java/com/marketplace/backend/config/CorsConfig.java
+++ backend/src/main/java/com/marketplace/backend/config/CorsConfig.java
@@ -2,39 +2,64 @@ package com.marketplace.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
+        CorsConfiguration configuration =
+                new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173",
-                "https://marketplace-campus-tan.vercel.app"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOrigins(
+                List.of(
+                        "http://localhost:5173",
+                        "https://marketplace-campus-tan.vercel.app"
+                )
+        );
+
+        configuration.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "PATCH",
+                        "OPTIONS"
+                )
+        );
+
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
 
         return source;
     }
+
+    @Override
     public void addResourceHandlers(
             ResourceHandlerRegistry registry) {
 
         registry
                 .addResourceHandler("/uploads/**")
-                .addResourceLocations(
-                        "file:uploads/"
-                );
+                .addResourceLocations("file:uploads/");
     }
 }
