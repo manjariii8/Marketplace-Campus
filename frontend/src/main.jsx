83914fc80a9c7d391e6117d7326eaa9c07096d81
Manjari <manjari.tripathi.tech@gmail.com>
2026-09-07 09:35:08 +0530

    Add Scroll to Top functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,14 @@ import App from "./App";
 import "./index.css";
 
 import { AuthProvider } from "./context/AuthContext";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <AuthProvider>
         <Toaster position="top-right" reverseOrder={false} />
 
